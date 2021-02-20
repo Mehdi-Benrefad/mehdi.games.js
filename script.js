1435596c89b869a98a0c71c0b7014b1488ad22d5
Mehdi-Benrefad <benrefadmehdi@gmail.com>
2021-02-20 16:06:02 +0100

    edit js

--- script.js
+++ script.js
@@ -19,6 +19,26 @@ function animatebomb(){
     score.style.display="inline";
 }
 
+
+/*
+    var gameover = setInterval(function() {
+        let soliderTop = parseInt(window.getComputedStyle(solider).getPropertyValue("top"));
+        let bombeLeft = parseInt(window.getComputedStyle(bombe).getPropertyValue("left"));
+        if(bombeLeft<20 && bombeLeft>-20 && soliderTop>=130){
+            bombe.style.animation = "none";
+            alert("Game Over. score: "+Math.floor(counter));
+            counter=0;
+            score.style.display="none";
+            bombe.classList.remove("animatebomb");
+            
+        }else{
+            counter++;
+            document.getElementById("score").innerHTML = Math.floor(counter);
+        }
+    }, 10);
+*/
+
+
 var gameover = setInterval(function() {
     let soliderTop = parseInt(window.getComputedStyle(solider).getPropertyValue("top"));
     let bombeLeft = parseInt(window.getComputedStyle(bombe).getPropertyValue("left"));
